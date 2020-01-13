b5967784d2b9b255cc13a73adc5e33c110e65334
martycistudent <marty.wickham@gmail.com>
2020-01-13 16:06:50 +0000

    Finished this python/mysql lesson

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get our username from the Gitpod workspace. Username is stores as an eniornment variable
@@ -9,16 +10,18 @@ username = os.getenv('GITPOD_USER')
 # Connect to the database (Our connection object)
 connection = pymysql.connect(host='localhost',
                              user=username,
-                             password='',
+                             password='', 
                              db='Chinook')
 
 try:
     # Run a query
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"   
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)           
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        list_of_names = ['fred', 'Fred']
+        # Prepare a string with the same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
+        connection.commit() # Saves the changes to your table
+  
 finally:
     # Close the connection, regardless of whether the above was successful or not
     connection.close()
